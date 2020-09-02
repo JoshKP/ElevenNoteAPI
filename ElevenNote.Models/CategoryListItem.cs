8448b01dba4f8ef3004c3c6b7742792a534ab755
Josh <joshpiersimoni@gmail.com>
2020-09-02 14:58:50 -0400

    created category controller for webapi

--- ElevenNote.Models/CategoryListItem.cs
+++ ElevenNote.Models/CategoryListItem.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    class CategoryListItem
+    public class CategoryListItem
     {
+        public int CategoryId { get; set; }
+        [Display(Name = "Category")]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(25, ErrorMessage = "There are too many characters in this field.")]
+        public string CategoryName { get; set; }
     }
 }
--- ElevenNote.Services/CategoryService.cs
+++ ElevenNote.Services/CategoryService.cs
@@ -21,9 +21,34 @@ namespace ElevenNote.Services
             var entity =
                 new Category()
                 {
-                    OwnderId = _userId,
                     CategoryName = model.CategoryName
-                }
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CategoryListItem> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Categories
+                        .Select(
+                        e =>
+                            new CategoryListItem
+                            {
+                                CategoryId = e.CategoryId,
+                                CategoryName = e.CategoryName,
+                            }
+                        );
+
+                return query.ToArray();
+            }
         }
     }
 }
